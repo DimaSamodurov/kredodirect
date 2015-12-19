d3ea3d3dc980f8397ec9f3a9b6c544787018a082
Dima Samodurov <dimasamodurov@gmail.com>
2015-12-20 01:53:34 +0200

    Keep only required options in firefox config

--- firefox_config.rb
+++ firefox_config.rb
@@ -6,11 +6,17 @@ Capybara.register_driver :firefox do |app|
 
   # Save downloads automatically to specified folder.
   profile['browser.download.folderList'] = 2
-  profile['browser.download.dir'] = File.expand_path(File.join(__dir__, 'tmp/download'))
-#  profile['browser.helperApps.alwaysAsk.force'] = false
-#  profile['browser.download.manager.showWhenStarting']= false
+  profile['browser.download.dir'] = File.expand_path(File.join(__dir__, 'tmp/downloads'))
+  profile['browser.helperApps.alwaysAsk.force'] = false
+  profile['browser.download.manager.showWhenStarting']= false
 
   # Suppress "open with" dialog
-  profile['browser.helperApps.neverAsk.saveToDisk'] = 'application/vnd.ms-excel'
+  profile['browser.helperApps.neverAsk.saveToDisk'] = 'application/vnd.ms-excel, text/html'
   Capybara::Selenium::Driver.new(app, :browser => :firefox, :profile => profile)
+end
+
+Capybara.configure do |config|
+  config.run_server     = false
+  config.save_and_open_page_path = File.join(__dir__, 'tmp')
+  config.default_driver = :firefox
 end
