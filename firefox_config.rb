fb99b3b178d6cb07836d1c3f949869d043d5f46a
Dima Samodurov <dimasamodurov@gmail.com>
2015-12-19 22:31:14 +0200

    Configure Firefox to save files to specific folder.

--- firefox_config.rb
+++ firefox_config.rb
@@ -1,11 +1,16 @@
-Capybara.register_driver :selenium do |app|
+Capybara.register_driver :firefox do |app|
   profile = Selenium::WebDriver::Firefox::Profile.new
-  profile['browser.download.dir'] = '/tmp'
+
+  # Enable Webdriver extension which might be disable in firefox (case of Mac).
+  profile['xpinstall.signatures.required'] = false
+
+  # Save downloads automatically to specified folder.
   profile['browser.download.folderList'] = 2
-  profile["browser.helperApps.alwaysAsk.force"] = false
-  profile["browser.download.manager.showWhenStarting"]= false
+  profile['browser.download.dir'] = File.expand_path(File.join(__dir__, 'tmp/download'))
+#  profile['browser.helperApps.alwaysAsk.force'] = false
+#  profile['browser.download.manager.showWhenStarting']= false
 
   # Suppress "open with" dialog
-  profile['browser.helperApps.neverAsk.saveToDisk'] = ''
+  profile['browser.helperApps.neverAsk.saveToDisk'] = 'application/vnd.ms-excel'
   Capybara::Selenium::Driver.new(app, :browser => :firefox, :profile => profile)
 end
--- lib/kredodirect/extractor.rb
+++ lib/kredodirect/extractor.rb
@@ -5,21 +5,30 @@ module Kredodirect
 
     def run
       visit '/'
-      fill_in('client_id', with: '')
-      fill_in('password', with: '')
+      fill_in('client_id', with: username)
+      fill_in('password', with: password)
       find('.login_content .div_content table tr td table tr td input').click
-
 
       click_link('Поточний рахунок')
       click_link('Історія рахунку')
       click_link('Експорт в Excel')
 
-      # Read more of capybara functions here
-      # https://github.com/jnicklas/capybara#clicking-links-and-buttons
-      # click_link('')
-      #save_and_open_page
-      data = page.driver.html.force_encoding('utf-8')
-      File.binwrite('tmp/page.xls', data)
+      # Wait for download complete
+      sleep 5
+    end
+
+    private
+
+    def username
+      ENV.fetch('KREDO_USER') do
+        'KREDO_USER environment variable is not set.'
+      end
+    end
+
+    def password
+      ENV.fetch('KREDO_PASS') do
+        'KREDO_PASS environment variable is not set.'
+      end
     end
   end
 end
